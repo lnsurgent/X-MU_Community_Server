11298d623ba96bcd114f6b4a47f668bb8ffe9144
Евгений <fy_matrix@inbox.lv>
2015-01-19 13:25:29 +0200

    Delete CRC.h

--- zClient_X/zClient_S6E3/CRC.h
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-///////////////////////////////////////////////////////////////////
-
-void InitCrcTable();
-ULONG Reflect(ULONG ref, char ch);
-int Get_CRC(unsigned char* buffer, ULONG bufsize);
-long FileSize(FILE *input);
-int  GetCheckSumFromMain();
